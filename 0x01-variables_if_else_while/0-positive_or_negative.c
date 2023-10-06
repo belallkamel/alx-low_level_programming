7bdc9947169c3a197db55e91d85ed5237d4fac05
belallkamel <belal.kamel1000@gmail.com>
2023-10-06 16:05:42 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,16 +16,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
 		printf("%i is positive");
-	}
 	else if (n < 0)
-	{
 		printf("%i is negative");
-	}
 	else
-	{
-		printf("%i is zero")
-	}
+		printf("%i is zero");
 	return (0);
 }
